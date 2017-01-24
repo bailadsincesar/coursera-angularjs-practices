40bdcb8eab2170faf598be65b6139593f2b107ad
Cesar Alfredo Tarrillo Celada <cesar@Cesars-MacBook-Pro.local>
2017-01-24 00:42:19 -0500

    feat(categories list): implement request list of categories using $http service

--- promises-http/app.js
+++ promises-http/app.js
@@ -3,7 +3,9 @@
 
     angular.module('ShoppingList', [])
         .controller('ListController', ListController)
-        .service('EmailValidator', EmailValidatorService);
+        .controller('MenuController', MenuController)
+        .service('EmailValidator', EmailValidatorService)
+        .service('MenuCategoriesService', MenuCategoriesService);
 
     ListController.$inject = ['EmailValidator'];
     function ListController(EmailValidator) {
@@ -15,7 +17,8 @@
         self.addToList = function () {
             var promise = EmailValidator.validateEmail(self.email);
 
-            promise.then(function (response) {
+            promise
+            .then(function (response) {
                 self.error = response.message;
                 var contact = {
                     name: self.name,
@@ -23,7 +26,11 @@
                 };
                 self.contacts.push(contact);
 
-            }).catch(function (errorResponse) {
+            })
+            .then(function (otherResponse) {
+
+            })
+            .catch(function (errorResponse) {
                 self.error = errorResponse.message;
 
             });
@@ -36,8 +43,9 @@
         var self = this;
 
         self.validateEmail = function (email) {
+            console.log('enter validation');
             var deferred = $q.defer();
-
+            console.log('after defer');
             $timeout(function () {
                 var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                 var result = {
@@ -56,6 +64,38 @@
 
             return deferred.promise;
 
+        }
+    }
+
+
+    MenuController.$inject = ['$scope', 'MenuCategoriesService'];
+    function MenuController($scope, MenuCategoriesService) {
+        var promise = MenuCategoriesService.getMenuCategories();
+
+        promise.then(function (response) {
+            console.log(response.data);
+            $scope.categories = response.data;
+
+        }).catch(function (error) {
+            console.log(error.message);
+
+        });
+    }
+
+
+    MenuCategoriesService.$inject = ['$http'];
+    function MenuCategoriesService($http) {
+        var self = this;
+
+        self.getMenuCategories = function () {
+            var response = $http({
+                method: 'GET', //default is GET
+                url: 'http://davids-restaurant.herokuapp.com/categories.json', //the only mandatory
+
+            });
+
+            // $http returns a promise, so response is a promise
+            return response;
         }
     }
 
